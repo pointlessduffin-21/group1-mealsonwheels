264e0b2e3a3abae78ff6fb263cae3ae42befe506
pointlessduffin-21 <francisroelabarca12@gmail.com>
2023-07-07 22:00:10 +0800

    added repository

--- mealsonwheels [Checkout Working]/src/main/java/com/merrymeals/mealsonwheels/Repository/OrderRepository.java
+++ mealsonwheels [Checkout Working]/src/main/java/com/merrymeals/mealsonwheels/Repository/OrderRepository.java
@@ -7,24 +7,33 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.merrymeals.mealsonwheels.Entity.Meal;
 import com.merrymeals.mealsonwheels.Entity.Meal_Order;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Meal, Long>{
-
-	void save(Meal_Order order);
-
+public interface OrderRepository extends JpaRepository<Meal_Order, Long>{
 
 	@Query("SELECT o FROM Meal_Order o WHERE o.m_id = :mealId")
 	public Long getMealIdById(@Param("mealId") Long mealId);
-	
-	@Query(value = "SELECT user_details_id, first_name, last_name, title, about, phone_number, address, city, country FROM meal_order"
-			+ " JOIN meal ON meal_order.m_id = meal.m_id"
-			+ " WHERE meal.m_id = :mealId", nativeQuery = true)
-	public String getDetailsById(@Param("mealId") String userId);
-	
-	@Query(value = "SELECT order_number FROM Meal_Order ORDER BY mo_id DESC LIMIT 1")
+
+//	@Query(value = "SELECT user_details_id, first_name, last_name, title, about, phone_number, address, city, country FROM meal_order"
+//			+ " JOIN meal ON meal_order.m_id = meal.m_id"
+//			+ " WHERE meal.m_id = :mealId", nativeQuery = true)
+//	public String getDetailsById(@Param("mealId") String userId);
+
+	@Query(value = "SELECT order_number FROM Meal_Order ORDER BY mo_id DESC LIMIT 1",
+			nativeQuery = true)
 	public String getLastOrderNumber();
-	
+
+	@Query("SELECT mo FROM Meal_Order mo WHERE mo.status LIKE :ORDERED")
+	public List<Meal_Order> getOrdered();
+
+	@Query("SELECT mo FROM Meal_Order mo WHERE mo.status LIKE :APPROVED")
+	public List<Meal_Order> getApproved();
+
+	@Query("SELECT mo FROM Meal_Order mo WHERE mo.status LIKE :COOKED")
+	public List<Meal_Order> getCooked();
+
+	@Query("SELECT mo FROM Meal_Order mo WHERE mo.status LIKE :DELIVERED")
+	public List<Meal_Order> getDelivered();
+
 }
