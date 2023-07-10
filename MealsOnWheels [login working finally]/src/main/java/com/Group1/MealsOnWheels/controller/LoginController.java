bb5281c77b824f848d1ab15f8b726191f462f614
pointlessduffin-21 <francisroelabarca12@gmail.com>
2023-07-11 05:42:15 +0800

    Fixed the login with response

--- MealsOnWheels [login working finally]/src/main/java/com/Group1/MealsOnWheels/controller/LoginController.java
+++ MealsOnWheels [login working finally]/src/main/java/com/Group1/MealsOnWheels/controller/LoginController.java
@@ -1,18 +1,28 @@
 package com.Group1.MealsOnWheels.controller;
 
 import com.Group1.MealsOnWheels.Entity.User;
+import com.Group1.MealsOnWheels.repository.UserRepository;
 import com.Group1.MealsOnWheels.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
     @Autowired
     UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("login")
     public String onLogin() {
@@ -24,6 +34,7 @@ public class LoginController {
     public String onLoginError(Model model) {
         String error_msg = "Incorrect username or password. Try Again.";
         model.addAttribute("error_string", error_msg);
+        System.out.println(error_msg);
         return "login";
     }
 
@@ -46,14 +57,28 @@ public class LoginController {
     }
 
     @GetMapping("login_success")
-    public String logginSuccess() {
+    public String logginSuccess(HttpSession session) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            UserDetails loggedUser = (UserDetails) authentication.getPrincipal();
+            session.setAttribute("user", loggedUser);
+            System.out.println("User has logged in as: " + loggedUser.getUsername());
+        }
+
         return "index";
     }
 
     @GetMapping("/test")
     public String testEndpoint() {
-
         return "testy";
     }
+
+//    @GetMapping("/logout")
+//    public String logout(HttpServletRequest request) {
+//        request.getSession().invalidate();
+//        System.out.println("User has logged out.");
+//    	return "login";
+//    }
 
 }
--- MealsOnWheels [login working finally]/src/main/java/com/Group1/MealsOnWheels/repository/UserRepository.java
+++ MealsOnWheels [login working finally]/src/main/java/com/Group1/MealsOnWheels/repository/UserRepository.java
@@ -8,6 +8,10 @@ public interface UserRepository extends JpaRepository<User, Long>{
 
     User findByUserName(String name);
 
+    @Query(value="SELECT * FROM user WHERE user_name = :email AND password = :password",
+            nativeQuery = true)
+    public User loginUser(String email, String password);
+
     @Query(value="SELECT role.name FROM user JOIN user_role ON user.u_id = user_role.user_id JOIN role ON user_role.role_id = role.id WHERE user.u_id = :uid",
             nativeQuery = true)
     public String findRoleByUid(Long uid);
